201de3a14889cb05f45b0897f95fd13a5e1afcfa
wuwilliam2049 <wuwilliam2049@gmail.com>
2017-03-13 20:24:45 -0700

    add google calculator feature

---
+++ app/src/main/java/com/williamwu2049/calculator/CalculatorEditText.java
@@ -0,0 +1,15 @@
+package com.williamwu2049.calculator;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by William on 3/13/17.
+ */
+
+class CalculatorEditText extends View {
+    public CalculatorEditText(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
